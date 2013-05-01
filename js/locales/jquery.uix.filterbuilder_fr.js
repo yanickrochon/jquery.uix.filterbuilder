570e572735c3c9284d9cf55cf8d92df9afbba599
Yanick Rochon <yanick.rochon@gmail.com>
2013-05-01 20:09:08 -0300

    Create jquery.uix.filterbuilder_fr.js

---
+++ js/locales/jquery.uix.filterbuilder_fr.js
@@ -0,0 +1,51 @@
+/*
+ * jQuery UIx FilterBuilder
+ *
+ * Authors:
+ *  Yanick Rochon (yanick.rochon[at]gmail[dot]com)
+ *
+ * Dual licensed under the MIT (MIT-LICENSE.txt)
+ * and GPL (GPL-LICENSE.txt) licenses.
+ *
+ *
+ * Localization : FR
+ *
+ */
+
+(function ($) {
+
+  $.uix.filterbuilder.i18n['fr'] = {
+		"expr.match.all": "Tous",
+		"expr.match.any": "N'importe quel",
+		"expr.match.none": "Aucun",
+
+		"field.equal.field": "égal au champ",
+		"field.equal": "égal à",
+		"field.not.equal": "pas égal à",
+		"field.match": "correspond à",
+		"field.not.match": "différent de",
+		"field.null": "est null",
+		"field.not.null": "n'est pas null",
+		"field.between": "entre (exclusif)",
+		"field.between.inc": "entre (inclusif)",
+		"field.contain": "contient",
+		"field.not.contain": "ne contient pas",
+		"field.start.with": "début avec",
+		"field.not.start.with": "ne début pas avec",
+		"field.end.with": "termine avec",
+		"field.not.end.with": "ne termine pas avec",
+		"field.less.than": "plus petit que",
+		"field.less.than.equal": "plus petit ou égal à",
+		"field.greater.than": "plus grand que",
+		"field.greater.than.equal": "plus grand ou égal à",
+
+		"btn.add": "Ajouter",
+		"btn.add.sub": "Ajouter sous",
+		"btn.remove": "Enlever"
+	};
+
+	// link locales
+	$.uix.filterbuilder.i18n['fr_CA'] = $.uix.filterbuilder.i18n['fr_FR'] = $.uix.filterbuilder.i18n['fr'];
+	// ...
+
+})(jQuery);
